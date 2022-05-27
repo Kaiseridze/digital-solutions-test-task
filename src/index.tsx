3295bed0e42cf6b20a385535087d84453ca80a5b
Kaiseridze <kaiseidze@gmail.com>
2022-05-27 15:19:21 +0300

    add baseurl

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<Router>
+			<Router basename={process.env.PUBLIC_URL}>
 				<App />
 			</Router>
 		</Provider>
